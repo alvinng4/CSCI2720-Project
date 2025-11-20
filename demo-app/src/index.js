66c81bc0de95daf203f046c672ab159cab172880
LiClement0216 <clementli0216@gmail.com>
2025-11-21 07:18:03 +0800

    Update demo-app

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -3,6 +3,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 
+let locationData = [
+    {id: 1, location:"location1", distance: 61, noOfEvents: 3}, 
+    {id: 2, location:"location2", distance: 13, noOfEvents: 4}, 
+    {id: 3, location:"location3", distance: 71, noOfEvents: 6}, 
+];
+
 class App extends React.Component {
   render() {
     return (
@@ -26,10 +32,85 @@ class LocationList extends React.Component {
     return (
       <div className='body'>
         <h2>LocationList</h2>
+        <div className='locationCardsTitle'>
+          <table>
+            <tr>
+              <th style={{width: "140px"}}>
+                <div>ID</div>
+              </th>
+              <th style={{width: "240px"}}>
+                <div>Location</div>
+              </th>
+              <th style={{width: "140px"}}>
+                <div>DISTANCE</div>
+              </th>
+              <th style={{width: "240px"}}>
+                <div>NUMBER OF EVENTS</div>
+              </th>
+              <th style={{width: "240px"}}>
+                <div>Add to favourite</div>
+              </th>
+            </tr>
+          </table>
+        </div>
+        
+        <div className='locationCardsTable'>
+          <>
+            {locationData.map((file,index) => <LocationCard i={index} key={index}/>)}
+          </>
+        </div>
+        
       </div>
     );
   }
 }
+
+
+    
+class LocationCard extends React.Component{
+    // Add your code here
+    // Use Bootstrap cards
+    render() {
+        let i = this.props.i;
+        return (
+            // change data[0] to data[i]
+        <>
+          <hr />
+          <div>
+            <table className='locationCards'>
+              <tr>
+                <th style={{width: "140px"}}>
+                  <div>{locationData[i].id}</div>
+                </th>
+                <th style={{width: "240px"}}>
+                  <div className='locationNameBoxes'>{locationData[i].location}</div>
+                </th>
+                <th style={{width: "140px"}}>
+                  <div>{locationData[i].distance} km</div>
+                </th>
+                <th style={{width: "240px"}}>
+                  <div>{locationData[i++].noOfEvents}</div>
+                </th>
+                <th style={{width: "240px"}}>
+                  <button className='favouriteButton' >✓</button>
+                </th>
+              </tr>
+            </table>
+          </div>
+        </>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 class EventList extends React.Component {
   render() {
@@ -113,6 +194,10 @@ class Navigation extends React.Component {
     );
   }
 }
+
+
+
+
 
 const root = ReactDOM.createRoot(document.querySelector('#app'));
 root.render(<App />);
