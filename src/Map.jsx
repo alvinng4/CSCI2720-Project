9a58e1b7951cc50d9f87bcb00b71f07003ae8538
LiClement0216 <clementli0216@gmail.com>
2025-12-04 07:51:36 +0800

    Add openstreetmap

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,11 +1,33 @@
 import { PageShell } from "@/components/page-shell"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import locationsData from "/data/venues_cleaned.json";
 
 export function Map() {
+  const locations = locationsData.venues.venue;
+  const center = [locations[0].latitude, locations[0].longitude];
   return (
     <PageShell title="Map">
-      <p className="text-muted-foreground">
-        Hello, world!
-      </p>
+      <div className="flex items-center gap-x-2">
+        <MapContainer
+          center={center}
+          zoom={11}
+          style={{ height: "700px", width: "80%"}}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='© OpenStreetMap contributors'
+          />
+          {locations.map((loc) => (
+            <Marker key={loc._id} position={[loc.latitude, loc.longitude]}>
+              <Popup>{loc.venuee}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+        
+      </div>
+      
+      
     </PageShell>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from "@/lib/AuthContext";
+import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
