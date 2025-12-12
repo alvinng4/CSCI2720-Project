b25866faccc10da9999665ce4b72b9f8812ad84c
Alvin <154572722+alvinng4@users.noreply.github.com>
2025-12-12 23:18:53 +0800

    Fix map crash when no location data

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -68,7 +68,7 @@ export function Map() {
           <div className="w-full">
             <MapComponent
               locations={filteredLocations}
-              center={[locations[0]?.latitude, locations[0]?.longitude]}
+              center={[locations[0]?.latitude ?? 22.3, locations[0]?.longitude ?? 114.2]}
               onClick={(loc) => setSelectedLocation(loc)}
               style={{ height: "700px", width: "100%", zIndex: "1" }}
             />
