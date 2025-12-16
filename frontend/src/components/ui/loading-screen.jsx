046fc960c65bd0c24c3cbd39f35ca2d327d3f7a9
Alvin <154572722+alvinng4@users.noreply.github.com>
2025-12-16 23:06:03 +0800

    Update loading-screen.jsx

--- frontend/src/components/ui/loading-screen.jsx
+++ frontend/src/components/ui/loading-screen.jsx
@@ -1,6 +1,6 @@
 export function LoadingScreen() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+    <div className="fixed inset-0 flex items-center justify-center pointer-events-none">
       <div className="sk-chase">
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
